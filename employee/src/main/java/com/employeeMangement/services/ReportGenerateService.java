a7e526eb551fa7bb1c1bfea1a1b7d7d3a44c2062
maheshbaldaniya <maheshbaldaniya@vebuin.com>
2024-12-31 21:34:57 +0530

    push changes

--- employee/src/main/java/com/employeeMangement/services/ReportGenerateService.java
+++ employee/src/main/java/com/employeeMangement/services/ReportGenerateService.java
@@ -24,26 +24,35 @@ public class ReportGenerateService {
 
     public void generateEmployeeReport() throws JRException, IOException {
         // Step 1: Fetch employees from the database
-        Optional<Department> departmentOptional = departmentRepository.findById(1l);
-        if (!departmentOptional.isPresent()) {
-            throw new RuntimeException("Department not found with ID: " + 1l);
-        }
+        List<Department> departmentList  = departmentRepository.findAll();
+//        if (!departmentOptional.isPresent()) {
+//            throw new RuntimeException("Department not found with ID: " + 1l);
+//        }
+//
+//        Department department = departmentOptional.get();
+//
+//        // Prepare data for the report
+//        List<Department> departmentList = Collections.singletonList(department);
 
-        Department department = departmentOptional.get();
-
-        // Prepare data for the report
-        List<Department> departmentList = Collections.singletonList(department);
-
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(departmentList);
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(departmentList.get(0).getEmployees());
 
         // Load the JRXML file
-        ClassPathResource reportResource = new ClassPathResource("reports/employee_report.jrxml");
+        ClassPathResource reportResource = new ClassPathResource("reports/department_master_report.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(reportResource.getInputStream());
+
+//        ClassPathResource subReportResource = new ClassPathResource("reports/employee_subreport.jrxml");
+//        JasperReport subReport = JasperCompileManager.compileReport(subReportResource.getInputStream());
+
 
         // Set parameters
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("ReportTitle", "department Report");
+        // Prepare parameters
+//        parameters.put("departmentName", department.getName());
+//        parameters.put("departmentLocation", department.getLocation());
+        parameters.put("departmentList", departmentList);
 
+//        parameters.put("SubreportPath", subReport); // Pass the compiled subreport
         // Fill the report with data
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 
